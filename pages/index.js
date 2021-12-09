aeb8a8c9490c53afd0271475f92ebe31d5dad70b
JohnWebKilla <johnganiev95@gmail.com>
2021-12-09 22:55:10 +0500

    new

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
-import firebase from "../firebase/firebase";
-import Router from "next/router";
-import { Card } from 'antd';
-import 'antd/dist/antd.css';
+import Header from "../components/header";
 
 export default function Home() {
 
-  useEffect(() => {
-    if (!firebase.isLoggedIN()) {
-      Router.push("/login");
-    }
-  });
 
   return (
     <>
-  <Card
-    hoverable
-    style={{ width: 240 }}
-    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-  >
-    <Meta title="Europe Street beat" description="www.instagram.com" />
-  </Card>,
+<Header />
 </>
 
     )
