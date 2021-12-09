ca6860af2d1b7b088e9736c601daaac7e4aa1236
JohnWebKilla <johnganiev95@gmail.com>
2021-12-09 22:50:07 +0500

    new

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import firebase from "../firebase/firebase";
 import Router from "next/router";
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
 
 export default function Home() {
 
@@ -12,7 +14,13 @@ export default function Home() {
 
   return (
     <>
-  
+  <Card
+    hoverable
+    style={{ width: 240 }}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>,
 </>
 
     )
